bbfb1e50e9da51c16a32859383cdb19d02029a61
nehajha2804 <nehajha2804@gmail.com>
2026-04-08 01:40:26 +0530

    Update and rename Array/duplicateDetection.java to removeNonZero.java

--- removeNonZero.java
+++ removeNonZero.java
@@ -1,11 +1,11 @@
-class duplicateDetection{
+class removeNonZero{
   int slow=0;
 for(int fast=0; fast<arr.length; fast++){
 if(arr[fast]!=0){
 //swap(fast,slow)
 int temp=arr[fast];
 arr[fast]=arr[slow];
-arr[fast]=temp;
+arr[slow]=temp;
 slow++;
 }
 }
