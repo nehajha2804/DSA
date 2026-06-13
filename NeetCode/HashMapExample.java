f79fbbc4f3ca93348a2f97c762f5ade077d101c6
Neha <nehajha2804@gmail.com>
2026-06-13 23:54:11 +0530

    Changed name HashMapExample

---
+++ NeetCode/HashMapExample.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+public class HashMapExample{
+    public static void main(String args[]){
+        HashMap <Integer, Integer> map = new HashMap <>();
+    
+        map.put(8,0);
+
+       
+    }
+}
+
+ // map.put(key, value);
+        // map.get(key);
+        // map.getOrDefault(key, 0);
+        // map.containsKey(key);
+        // map.remove(key);
+        // map.keySet();
+        // map.entrySet();
