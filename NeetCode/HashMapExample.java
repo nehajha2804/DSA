add87fc6e2584fb1dd6c4e539f5b522d4766fb41
Neha <nehajha2804@gmail.com>
2026-06-14 00:47:43 +0530

    practised functions of HashMap

--- NeetCode/HashMapExample.java
+++ NeetCode/HashMapExample.java
@@ -2,17 +2,29 @@ import java.util.HashMap;
 public class HashMapExample{
     public static void main(String args[]){
         HashMap <Integer, Integer> map = new HashMap <>();
-    
+        //map.put(key,value) is used to add a key-value pair in the HashMap
         map.put(8,0);
+        map.put(9,1);
+        map.put(10,1);
 
-       
-    }
-}
+        // map.get(key) returns value associated with the key associated with the value
+        System.out.println(map.get(8));
 
- // map.put(key, value);
-        // map.get(key);
-        // map.getOrDefault(key, 0);
+
+        /* map.getOrDefault(key, 0) gets the 
+        value of corresponding to the key if present. else return the 
+        given default value*/
+        System.out.println(map.getOrDefault(10,0)); //here the default value is 0
+        System.out.println(map.getOrDefault(11,1)); //here the default value is 1
+
         // map.containsKey(key);
+        //map.containsValue(Value;
+        System.out.println(map.containsValue(1));
         // map.remove(key);
         // map.keySet();
         // map.entrySet();
+
+    }
+}
+
+       
