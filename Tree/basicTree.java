de773d38e0543670dd8c1db536af808ebc8ccf3a
Neha <nehajha2804@gmail.com>
2026-06-16 20:07:28 +0530

    Added inOrder, preOrder, postOrder

--- Tree/basicTree.java
+++ Tree/basicTree.java
@@ -1,28 +1,63 @@
+public class basicTree{
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(5);
+
+        // System.out.println(root.val);
+        // System.out.println(root.left.val);
+        // System.out.println(root.right.val);
+
+        // inOrder(root);
+        // preOrder(root);
+        postOrder(root);
+
+
+    }
+
+     public static void inOrder(TreeNode root){
+        //left root right
+        if(root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.val + " ");
+        inOrder(root.right);
+    
+}
+
+    public static void preOrder(TreeNode root){
+        //root left right
+        if(root == null){
+            return;
+        }
+        
+        System.out.print(root.val + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void postOrder(TreeNode root){
+        //left right root
+        if(root == null){
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.val + " ");
+
+    }
+}
+
 class TreeNode{
-    int value;
+    int val;
     TreeNode left;
     TreeNode right;
 
-    TreeNode(int value){
-        this.value = value;
-    }
-
-   
-}
-
-public class basicTree{
-    public static void main(String args[]){
-
-        TreeNode root = new TreeNode(5);
-        root.left = new TreeNode(3);
-        root.right = new TreeNode(4);
-        root.left.left= new TreeNode(8);
-        root.left.right= new TreeNode(6);
-        root.right.left= new TreeNode(7);
-        root.right.right= new TreeNode(0);
-
-        System.out.println(root.toString());
-
+    TreeNode(int val){
+        this.val = val;
     }
 }
+
+
 
