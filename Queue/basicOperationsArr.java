6810882b8d117bca8ef5e9430a60a128337abc2a
Neha <nehajha2804@gmail.com>
2026-06-21 00:06:19 +0530

    Created a new Queue Folder

---
+++ Queue/basicOperationsArr.java
@@ -0,0 +1,15 @@
+public class basicOperationsArr{
+    public static void main(String args[]){
+
+    }
+}
+
+class QueueArray{
+    int queue[];
+    int front;
+    int rear;
+    int capacity;
+
+    
+    
+}
