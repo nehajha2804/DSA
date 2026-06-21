50243713e8537ddb5748a3cebbd35cde91c073b8
Neha <nehajha2804@gmail.com>
2026-06-21 14:44:57 +0530

    Halted current work on Queue

--- Queue/basicOperationsArr.java
+++ Queue/basicOperationsArr.java
@@ -1,15 +1,25 @@
 public class basicOperationsArr{
     public static void main(String args[]){
 
+        QueueArray queue = new QueueArray(10);
+        
+
     }
 }
 
 class QueueArray{
-    int queue[];
+    int queueArr[];
     int front;
     int rear;
-    int capacity;
+    int capacity; //max no of elements
+    int size;   //min number of elements
 
-    
+    QueueArray(int capacity){
+        this.capacity = capacity;
+        front = -1;
+        rear = 0;
+        size = 0;
+        queueArr = new int [capacity];
+    }
     
 }
