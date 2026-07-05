8628cfb9feb74553212c0d24c3e9491c57f712a8
Neha <nehajha2804@gmail.com>
2026-07-05 20:41:44 +0530

    list data structure created

---
+++ List/basicSyntax.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class basicSyntax{
+    public  static  void main(String args[]){
+        List<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(30);
+        list.add(40);
+    }
+}
