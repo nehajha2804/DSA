20d42c750f0452b2f7e0c9aa2be024849fbdf103
Neha <nehajha2804@gmail.com>
2026-07-10 17:46:19 +0530

    Added the anangram code

--- Array&HashMap/groupAnanagram.java
+++ Array&HashMap/groupAnanagram.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class groupAnanagram{
     public static void main(String args[]){
         String strs[] = {"act","pots","tops","cat","stop","hat"};
-        
+        System.out.println(isAnangram(strs));
+       
+    }
+
+    public static List<List<String>> isAnangram (String strs[]){
+        HashMap <String, List<String>> map = new HashMap<>();
+
+        for(String str : strs){
+            char s[] = str.toCharArray();
+            Arrays.sort(s);
+            String sorted = new String(s);
+
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<>());
+            }
+
+            map.get(sorted).add(str);
+        }
+
+        return new ArrayList<>(map.values());
     }
 }
