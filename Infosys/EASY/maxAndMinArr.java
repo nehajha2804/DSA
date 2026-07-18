c9b6364199e65f4fd2ba299a74035bab11f633ae
Neha <nehajha2804@gmail.com>
2026-07-18 20:49:04 +0530

    Added maxAndMin in an Array

---
+++ Infosys/EASY/maxAndMinArr.java
@@ -0,0 +1,10 @@
+public class maxAndMinArr{
+    public static void main(String args[]){
+        int nums[] = {1,3,2,5,6,7};
+    }
+
+    public static int[] maxAndMin(int nums[]){
+        return new int[]{-1,-1};
+        
+    }
+}
