be7a0c181612b3c2c36531037c09a248c8619876
Neha <nehajha2804@gmail.com>
2026-07-18 21:21:24 +0530

    Added code in maxAndMinArr

--- Infosys/EASY/maxAndMinArr.java
+++ Infosys/EASY/maxAndMinArr.java
@@ -1,10 +1,24 @@
+import java.util.Arrays;
+
 public class maxAndMinArr{
     public static void main(String args[]){
         int nums[] = {1,3,2,5,6,7};
+
+        System.out.print(Arrays.toString(maxAndMin(nums)));
     }
 
     public static int[] maxAndMin(int nums[]){
-        return new int[]{-1,-1};
-        
+        int answer[] = new int[2];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            min = Math.min(nums[i], min);
+            max = Math.max(nums[i], max);
+        }
+        answer[0] = min;
+        answer[1] = max;
+
+        return answer;
+
     }
 }
