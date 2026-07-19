a71f9e5b43ae46551f78c7873ac87c6c6b378cdb
Neha <nehajha2804@gmail.com>
2026-07-19 17:36:17 +0530

    modified valid anangram

--- Infosys/EASY/validAnangram.java
+++ Infosys/EASY/validAnangram.java
@@ -4,6 +4,7 @@ public class validAnangram{
     public static void main(String args[]){
         String S= "racecar";
         String T= "carrace";
+        System.out.println(isAnangram(S,T));
     }
 
     public static boolean isAnangram(String S, String T){
